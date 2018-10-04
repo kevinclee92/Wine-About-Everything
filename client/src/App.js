4ff4969059f599f9769bc732ee1511f5a0a23f25
Armando <xxnowonxx@gmail.com>
2018-10-03 20:25:32 -0700

    setup for material-ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { Component } from "react";
 import Books from "./pages/Books";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import {
   BrowserRouter as Router,
   Route,
@@ -11,18 +13,43 @@ import {
   Redirect
 } from "react-router-dom";
 
-const App = () => (
-  <Router>
-    <div>
-      <Nav />
-      <Switch>
-        <Route exact path="/" component={Books} />
-        <Route path="/books" component={Books} />
-        <Route path="/books/:id" component={Detail} />
-        <Route component={NoMatch} />
-      </Switch>
-    </div>
-  </Router>
+import HomePage from "./pages/HomePage";
+import UserPage from "./pages/UserPage";
+
+class App extends Component {
+
+  state = {
+    user: {
+      loggedIn: false,
+      username: null,
+    }
+  }
+
+  fetchUser = () => {
+    return true
+  }
+
+  componentDidMount() {
+    this.fetchUser()
+    .then(user => this.setState({ user: { loggedIn: true }}))
+  }
+
+  render() {
+    return (
+
+      <Router>
+        <div>
+          <Nav />
+          <Header />
+            <Switch>
+              <Route exact path="/" component={HomePage} />
+              <Route path="/user/:username" component={UserPage} />
+              <Route component={NoMatch} />
+            </Switch>
+        </div>
+        <Footer />
+    </Router>
 );
+}}
 
 export default App;
---
+++ client/src/components/Header/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+const Header = () => (
+  <header>
+    <AppBar position="static">
+        <Toolbar>
+          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="title" color="inherit" className={classes.grow}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+  </header>
+);
+
+export default Header;
