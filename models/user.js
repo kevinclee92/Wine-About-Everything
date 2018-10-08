24742ca76effe69a10f3a6edb8e84b0c5694781c
Kevin Lee <kevinclee92@gmail.com>
2018-10-08 13:10:34 -0700

    passport server and local strategy

--- models/user.js
+++ models/user.js
@@ -1,10 +1,13 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
 var Schema = mongoose.Schema;
+
 // var passportLocalMongoose = require('passport-local-mongoose');
 
 const UserSchema = new Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
+  name: {type: String, required: true},
   phone: {type: String},
   street: {type: String},
   city: {type: String},
@@ -14,6 +17,14 @@ const UserSchema = new Schema({
   age: {type: Number},
   date: { type: Date, default: Date.now }
 });
+
+UserSchema.methods.hashPassword = function (password) {
+  return bcrypt.hashSync(password,bcrypt.genSaltSync(10))
+}
+
+UserSchema.methods.comparePassword = function (password,hash) {
+  return bcrypt.compareSync(password,hash)
+}
 
 // UserSchema.plugin(passportLocalMongoose);
 let User = mongoose.model("User", UserSchema);
