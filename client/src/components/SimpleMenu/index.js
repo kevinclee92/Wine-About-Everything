33d1c5a361d9926ae588f7c1f49d719d757c646b
Armando <xxnowonxx@gmail.com>
2018-10-23 00:24:49 -0700

    updated user page styling, cleared some unused items

--- client/src/components/SimpleMenu/index.js
+++ client/src/components/SimpleMenu/index.js
@@ -3,7 +3,7 @@ import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import MenuIcon from '@material-ui/icons/Menu';
-import MenuStyle from './SimpleMenu.css';
+import './SimpleMenu.css';
 
 class SimpleMenu extends React.Component {
   state = {
--- client/src/pages/Wine101/index.js
+++ client/src/pages/Wine101/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import Jumbotron from '../../components/Jumbotron'
 import img from '../../images/foodandwinepairing.jpg'
-import JumbotronImage from '../../components/JumbotronImage'
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import imgThree from '../../images/winestats.jpg';
