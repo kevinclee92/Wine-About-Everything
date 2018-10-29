72cafcab21e04b885f0a097a6e25ce2394ffaa2a
Armando <xxnowonxx@gmail.com>
2018-10-29 11:30:12 -0700

    updated routes index.js

--- routes/index.js
+++ routes/index.js
@@ -15,7 +15,7 @@ var isAuthenticated = function (req, res, next) {
   router.use("/api", apiRoutes);
 
   // If no API routes are hit, send the React app
-  router.use(function(req, res) {
+  router.use('*', function(req, res) {
     res.sendFile(path.join(__dirname, "../client/public/index.html"));
   });
 
