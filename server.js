37533eae933c8bfb47d3a6d31b91a8dad10342cc
Armando <xxnowonxx@gmail.com>
2018-11-04 21:41:20 -0800

    heroku deploy changes

--- server.js
+++ server.js
@@ -28,11 +28,11 @@ require('./config/passport.js')(passport);
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));  
-  app.get(/^\/(?!api).*/, (req, res) => { // don't serve api routes to react app
-    res.sendFile(path.join(__dirname, './client/build/index.html'));
-  });
-  console.log('Serving React App...');
 }
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 // Add routes, both API and view
 app.use(routes)
 
